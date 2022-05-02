7fe1c8b90047eab6ea0ef40d9453e969c1a40494
harry7337 <gumnurharish.201it223@nitk.edu.in>
2022-05-02 15:56:50 +0530

    Fixed GENERATE BILLING TRIGGER

--- GENERATE_BILLING_TRIGGER.sql
+++ GENERATE_BILLING_TRIGGER.sql
@@ -2,6 +2,8 @@
 -- Trigger Name: GENERATE_BILLING
 -- This trigger generates the bill and inserts a row in Billing_Details table
 -- -----------------------------------------------------------------------------------------
+DROP TRIGGER IF EXISTS GENERATE_BILLING;
+
 DELIMITER //
 CREATE TRIGGER GENERATE_BILLING
 AFTER UPDATE ON BOOKING_DETAILS
@@ -16,19 +18,23 @@ BEGIN
   DECLARE finalAmount FLOAT(10,2);
   IF IFNULL((NEW.ACT_RET_DT_TIME),'NULL') <> 'NULL' AND NEW.BOOKING_STATUS ='R' THEN
    
-    SELECT BILL_ID INTO lastBillId FROM ( SELECT BILL_ID, ROWNUM AS RN FROM  BILLING_DETAILS) 
-    WHERE RN= (SELECT MAX(ROWNUM) FROM BILLING_DETAILS);
+    SELECT x.BILL_ID INTO lastBillId FROM 
+      ( 
+        SELECT BILL_ID, ROWNUM AS RN FROM  BILLING_DETAILS
+      )x 
+    WHERE 
+      RN= (SELECT MAX(ROWNUM) FROM BILLING_DETAILS);
     
     SET newBillId = 'BL' || CHAR(CAST( SUBSTRING(lastBillId,3) AS UNSIGNED )+1);
     
-    CALCULATE_LATE_FEE_AND_TAX(NEW.ACT_RET_DT_TIME, NEW.RET_DT_TIME, NEW.REG_NUM,NEW.AMOUNT, totalLateFee, totalTax);
+    CALL CALCULATE_LATE_FEE_AND_TAX(NEW.ACT_RET_DT_TIME, NEW.RET_DT_TIME, NEW.REG_NUM,NEW.AMOUNT, totalLateFee, totalTax);
     
     SET totalAmountBeforeDiscount = NEW.AMOUNT + totalLateFee + totalTax;
     
-    CALCULATE_DISCOUNT_AMOUNT(NEW.DL_NUM, totalAmountBeforeDiscount, NEW.DISCOUNT_CODE, discountAmt);
+    CALL CALCULATE_DISCOUNT_AMOUNT(NEW.DL_NUM, totalAmountBeforeDiscount, NEW.DISCOUNT_CODE, discountAmt);
     
     SET finalAmount = totalAmountBeforeDiscount - discountAmt;  
-    --insert new bill into the billing_details table
+    -- insert new bill into the billing_details table
     INSERT INTO BILLING_DETAILS (BILL_ID,BILL_DATE,BILL_STATUS,DISCOUNT_AMOUNT,TOTAL_AMOUNT,TAX_AMOUNT,BOOKING_ID,TOTAL_LATE_FEE) 
     VALUES (newBillId,SYSDATE(),'P',discountAmt,finalAmount,totalTax,NEW.BOOKING_ID,totalLateFee);
   END IF;
